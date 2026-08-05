57864c900446cc02929df52b62aff6e442ed5d59
samitkumarpatel <samitkumarpatel@gmail.com>
2026-08-05 22:52:02 +0200

    removed unwanted dependency

--- src/main/java/net/samitkumar/multi_tenant_saloon_authz/MultiTenantSaloonAuthzApplication.java
+++ src/main/java/net/samitkumar/multi_tenant_saloon_authz/MultiTenantSaloonAuthzApplication.java
@@ -4,7 +4,6 @@ import org.jspecify.annotations.Nullable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
--- src/test/java/net/samitkumar/multi_tenant_saloon_authz/TestcontainersConfiguration.java
+++ src/test/java/net/samitkumar/multi_tenant_saloon_authz/TestcontainersConfiguration.java
@@ -1,7 +1,6 @@
 package net.samitkumar.multi_tenant_saloon_authz;
 
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
@@ -10,17 +9,9 @@ import org.springframework.security.oauth2.server.authorization.client.InMemoryR
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
-import org.testcontainers.postgresql.PostgreSQLContainer;
-import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
-
-    @Bean
-    @ServiceConnection
-    PostgreSQLContainer postgresContainer() {
-        return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
-    }
 
     @Bean
     RegisteredClientRepository registeredClientRepository() {
