958b66b90a2c2f95c0e195b9b21f8a78cbbd3c78
samitkumarpatel <samitkumarpatel@gmail.com>
2026-08-21 14:16:29 +0000

    remove email scope

--- src/main/java/net/samitkumar/multi_tenant_salon_authz/MultiTenantSalonAuthzApplication.java
+++ src/main/java/net/samitkumar/multi_tenant_salon_authz/MultiTenantSalonAuthzApplication.java
@@ -309,7 +309,7 @@ class NotificationService {
                 Use the link below to sign in (copy and paste into your browser if it does not open):
                 %s
 
-                If you did not request this, please report it to admin@my-salon.online
+                If you did not request this, please report it to admin@salonsaas.org
                 """.formatted(token, tokenLink);
 
         var htmlMessage = """
@@ -317,13 +317,13 @@ class NotificationService {
                 <p>Your one-time login token for <strong>My Salon</strong> is:</p>
                 <h2>%s</h2>
                 <p>Or sign in directly: <a href="%s">%s</a></p>
-                <p><small>If you did not request this, please contact <a href="mailto:admin@my-salon.online">admin@my-salon.online</a></small></p>
+                <p><small>If you did not request this, please contact <a href="mailto:admin@salonsaas.org">admin@salonsaas.org</a></small></p>
                 """.formatted(token, tokenLink, tokenLink);
         var request = new MailjetRequest(List.of(
                 new MailjetMessage(
                         mailFrom,
                         List.of(tto),
-                        "OTT from my-salon",
+                        "SaloonSaaS OTT",
                         textMessage,
                         htmlMessage
                 )
