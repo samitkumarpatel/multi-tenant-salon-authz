54fb50114925e3a0fe553312088dd427bcc50f70
samitkumarpatel <samitkumarpatel@gmail.com>
2026-08-31 21:33:46 +0200

    auth flow fix

--- src/main/java/net/samitkumar/multi_tenant_salon_authz/notification/NotificationService.java
+++ src/main/java/net/samitkumar/multi_tenant_salon_authz/notification/NotificationService.java
@@ -25,22 +25,33 @@ public class NotificationService {
 
         var textMessage = """
                 Welcome to SalonSaaS!
-                
-                Your one-time login token is: %s
-                
-                Use the link below to sign in (copy and paste into your browser if it does not open):
+
+                Your one-time login code is:
+
+                    %s
+
+                Go back to the tab where you started signing in and enter this code.
+                It is valid for a few minutes and can be used once.
+
+                If that tab is gone, you can also open this link in the SAME browser
+                you started from (it will not work in a different browser or device):
                 %s
-                
+
                 If you did not request this, please report it to admin@salonsaas.org
                 """.formatted(token, tokenLink);
 
         var htmlMessage = """
                 <p>Hi,</p>
-                <p>Your one-time login token for <strong>SalonSaaS</strong> is:</p>
-                <h2>%s</h2>
-                <p>Or sign in directly: <a href="%s">%s</a></p>
-                <p><small>If you did not request this, please contact <a href="mailto:admin@salonsaas.org">admin@salonsaas.org</a></small></p>
-                """.formatted(token, tokenLink, tokenLink);
+                <p>Your one-time login code for <strong>SalonSaaS</strong> is:</p>
+                <h2 style="font-size:28px;letter-spacing:4px;margin:12px 0">%s</h2>
+                <p>Go back to the tab where you started signing in and enter this code.
+                   It is valid for a few minutes and can be used once.</p>
+                <p><small>If that tab is gone, you can also
+                   <a href="%s">open this link in the same browser you started from</a>
+                   &mdash; it will not work in a different browser or device.</small></p>
+                <p><small>If you did not request this, please contact
+                   <a href="mailto:admin@salonsaas.org">admin@salonsaas.org</a></small></p>
+                """.formatted(token, tokenLink);
         var request = new MailjetRequest(List.of(
                 new MailjetMessage(
                         mailFrom,
